656846e287437b8af2d0a761babb19c751da73df
difusao <difusao@gmail.com>
2024-04-29 21:10:00 -0300

    update

---
+++ src/main/java/com/difusao/JavaSpringBootJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.difusao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/difusao/JavaSpringBootJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.difusao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
